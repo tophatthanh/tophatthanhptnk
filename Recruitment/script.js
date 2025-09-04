71d9f4445c1b1134236c2c02889478f4c4f6909a
moony <ncdh.moony@gmail.com>
2025-09-04 22:52:29 +0700

    Add website

---
+++ Recruitment/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// const subTitles = document.getElementsByClassName('subTitle');
+// const container = document.getElementsByClassName('container');
+
+// for(let i=0; i < subTitles.length; i++) {
+//     subTitles[i].addEventListener("click", function(){
+//         if(subTitles[i].className === "subTitle") {
+//             for(let j=0; j<subTitles.length; j++){
+//                 if(i != j){
+//                     subTitles[j].className = "subTitle";
+//                 }
+//             }
+//             subTitles[i].className = "subTitle active";
+//         } else if(subTitles[i].className === "subTitle active") {
+//             subTitles[i].className = "subTitle";
+//         }
+//     })
+// }
+$(document).ready(function(){
+    $(".subTitle").click(function() {
+        $(this).next().slideToggle(200);
+        $(this).toggleClass("active");
+
+        console.log(this.className);
+        if(this.className === "subTitle active"){
+            $(this).parent().siblings().children(".subTitle").next().slideUp(200);
+            $(this).parent().siblings().children(".subTitle").attr('class', 'subTitle');
+        }
+    }); 
+});
---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const menu = document.getElementById("menu");
+const navElements = document.getElementById("navElements");
+const navBar = document.getElementById("navBar");
+
+var lastY = 0;
+
+menu.addEventListener("click", function(){
+    if(navElements.className === "navElements"){
+        navElements.className = "navElements navElementsActive";
+        menu.innerHTML = "close";
+    } else {
+        navElements.className = "navElements";
+        menu.innerHTML = "menu";
+    }
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var curPos = window.scrollY;
+    if (curPos > 20) {
+        lastY = curPos;
+        navBar.className = "navBar navBarActive";
+    } else {
+        lastY = curPos;
+        navBar.className = "navBar";
+    }
+}
+
+const date = "28 Sep, 2025 23:59:59"
+const countdownDate = new Date(date).getTime();
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+function updateCountdown() {
+    var now = new Date().getTime();
+
+    var distance = countdownDate - now;
+
+    var d = Math.max(0, Math.floor(distance / (1000 * 60 * 60 * 24)));
+    var h = Math.max(0, Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    var m = Math.max(0, Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    var s = Math.max(0, Math.floor((distance % (1000 * 60) / 1000)));
+
+    function norm(a){
+        if(a < 10){
+            return '0' + a;
+        } else {
+            return '' + a;
+        }
+    }
+
+    days.innerHTML = norm(d);
+    hours.innerHTML = norm(h);
+    minutes.innerHTML = norm(m);
+    seconds.innerHTML = norm(s)
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
